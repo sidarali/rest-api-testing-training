fa50a60df2b2fd6c33005ce0ddf856bf93f1d1c5
eugene <eugene.rogowski.by@gmail.com>
2018-09-23 20:19:43 +0300

    auto-test-framework: - create module; - add Pet related models and constants;

---
+++ auto-test-framework/src/main/java/io/swagger/petstore/constant/PetStatus.java
@@ -0,0 +1,8 @@
+package io.swagger.petstore.constant;
+
+public class PetStatus {
+
+    public static final String AVAILABLE = "available";
+    public static final String PENDING = "pending";
+    public static final String SOLD = "sold";
+}
