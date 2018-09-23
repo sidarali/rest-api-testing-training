b52161c69f8585b27146fdb2511335b2156d3826
eugene <eugene.rogowski.by@gmail.com>
2018-09-23 21:42:32 +0300

    auto-test-framework: add specification for PetClient

---
+++ auto-test-framework/src/main/java/io/swagger/petstore/client/constant/Path.java
@@ -0,0 +1,12 @@
+package io.swagger.petstore.client.constant;
+
+public class Path {
+
+    //common
+    public static final String BASE_PATH = "/v2";
+
+    //pet
+    public static final String PET = "/pet";
+    public static final String FIND_BY_STATUS = "/findByStatus";
+    public static final String UPLOAD_IMAGE = "/uploadImage";
+}
