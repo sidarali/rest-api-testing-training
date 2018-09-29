a1050df855456c2ff53dd0a4a0e40685737602f4
eugene <eugene.rogowski.by@gmail.com>
2018-09-29 20:49:59 +0300

    auto-test-framework: add CreatePetTest;

---
+++ auto-test-framework/src/test/java/io/swagger/petstore/base/BaseTest.java
@@ -0,0 +1,5 @@
+package io.swagger.petstore.base;
+
+public abstract class BaseTest {
+
+}
---
+++ auto-test-framework/src/test/java/io/swagger/petstore/pet/CreatePetTest.java
@@ -0,0 +1,77 @@
+package io.swagger.petstore.pet;
+
+import io.swagger.petstore.base.BaseTest;
+import io.swagger.petstore.client.PetClient;
+import io.swagger.petstore.model.Category;
+import io.swagger.petstore.model.Pet;
+import io.swagger.petstore.model.Tag;
+import io.swagger.petstore.model.registry.PetRegistry;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.http.ContentType.JSON;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CreatePetTest extends BaseTest {
+
+    private int petId;
+
+    @AfterMethod
+    public void deletePet() {
+
+        PetClient.deletePet(petId);
+    }
+
+    @Test
+    public void testCreatePet_OnlyRequiredFields() {
+
+        Pet petToCreate = PetRegistry.getUniquePetWithSetRequiredFileds();
+        testCreatePet(petToCreate);
+    }
+
+    @Test
+    public void testCreatePet_AllFields() {
+
+        List<String> photoUrls = new ArrayList<>();
+        photoUrls.add("photos.org/photo1");
+        photoUrls.add("photos.org/photo2");
+
+        List<Tag> tags = new ArrayList<>();
+        tags.add(new Tag(1, "Test tag 1"));
+        tags.add(new Tag(2, "Test tag 2"));
+
+        Pet petToCreate = PetRegistry.getPetBuilder()
+                .setCategory(new Category(1, "Test category 1"))
+                .setPhotoUrls(photoUrls)
+                .setTags(tags)
+                .build();
+
+        testCreatePet(petToCreate);
+    }
+
+    private void testCreatePet(Pet petToCreate) {
+
+        petId = petToCreate.getId();
+
+        Pet createdPet = PetClient.createPet(petToCreate)
+                .assertThat()
+                .statusCode(200)
+                .contentType(JSON)
+                .extract().body().as(Pet.class);
+
+        assertThat(createdPet)
+                .isNotNull()
+                .isEqualTo(petToCreate);
+
+        Pet fetchedPet = PetClient.getPet(petId)
+                .assertThat().statusCode(200)
+                .extract().body().as(Pet.class);
+
+        assertThat(fetchedPet)
+                .isNotNull()
+                .isEqualTo(petToCreate);
+    }
+}
