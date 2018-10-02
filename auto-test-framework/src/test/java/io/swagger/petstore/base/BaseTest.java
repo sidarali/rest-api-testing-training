fa648e8f95e69eb1322a8e5e03e932efad42fc9e
eugene <eugene.rogowski.by@gmail.com>
2018-10-02 22:31:14 +0300

    auto-test-framework: modify BaseTest;

--- auto-test-framework/src/test/java/io/swagger/petstore/base/BaseTest.java
+++ auto-test-framework/src/test/java/io/swagger/petstore/base/BaseTest.java
@@ -1,10 +1,15 @@
 package io.swagger.petstore.base;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.swagger.petstore.model.Category;
 import io.swagger.petstore.model.Pet;
 import io.swagger.petstore.model.Tag;
 import io.swagger.petstore.data.PetStatus;
 import io.swagger.petstore.model.registry.PetRegistry;
+import org.testng.annotations.BeforeClass;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +19,15 @@ public abstract class BaseTest {
 
     protected static final String IMAGE_FILE_NAME = "bug.png";
     protected long nonexistent_id = ThreadLocalRandom.current().nextInt(1000, 2000);
+
+    @BeforeClass
+    public void setCommonRestAssuredConfigs() {
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                //enable for local development purposes
+//                .addFilter(new RequestLoggingFilter())
+//                .addFilter(new ResponseLoggingFilter())
+                .build();
+    }
 
     //helper methods
 
